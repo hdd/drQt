29177f7994fa7331a17509dd1941ce3b5b6524f9
langeli <langeli@hddlaptop.localdomain>
2010-12-23 21:16:18 +0000

    add autorefresh

--- src/drQueueCore.py
+++ src/drQueueCore.py
@@ -1,58 +1,88 @@
 import sys
 import os
 
-#from PyQt4.QtGui import *
+import time
+    
 from PyQt4 import QtCore
 from PyQt4 import QtGui
 
+#class Timer(threading.Thread):
+class Timer(QtCore.QThread):
+    def __init__(self,parent=None):
+        super(Timer,self).__init__(parent=parent)
+        self.mutex = QtCore.QMutex()
+        self.condition = QtCore.QWaitCondition()
+        self.runTime = 5
+        self.action=None
+        self._running=True
+        
+    def set_run_time(self,time=5):
+        self.runTime = time
+        
+    def run(self):
+        print "thread starting..." 
+        while True:
+            counter = self.runTime
+            for sec in range(self.runTime):
+                print "..tic..."
+                time.sleep(1.0)
+                counter -= 1
+                
+            self.emit(QtCore.SIGNAL("done"))
+
 class JobDataTab(QtCore.QObject):
-    def __init__(self,job_name=None):
+    
+    def __init__(self,drq_job_object=None):
         super(JobDataTab,self).__init__()
+        self._drq_job_object=drq_job_object
                 
         self.icons=[]
         self.columns=[]        
         
         self.current_path = os.path.dirname(__file__)
         
-        self.icons.append(QtGui.QPixmap(os.path.join(self.current_path,"icons","NO.png")))    
+        ##job_properties=["id","name","owner","status","process","left","done","pri","pool"]
+        
+        print drq_job_object.__dict__
+        
+        self.icons.append(QtGui.QPixmap(os.path.join(self.current_path,"icons","NONE.png")))  
+        self.icons.append(QtGui.QPixmap(os.path.join(self.current_path,"icons","NONE.png")))  
         self.icons.append(QtGui.QPixmap(os.path.join(self.current_path,"icons","OK.png")))
-        self.icons.append(QtGui.QPixmap(os.path.join(self.current_path,"icons","NONE.png")))       
-                        
-        self.properties={
-            "id": QtGui.QLabel(),
-            "name":QtGui.QLabel(),
-            "owner":QtGui.QLabel(),
-            "status":QtGui.QLabel(),
-            "process":QtGui.QLabel(),
-            "left":QtGui.QLabel(),
-            "done":QtGui.QLabel(),
-            "pri":QtGui.QLabel(),
-            "pool":QtGui.QLabel(),
-            }
-   
-        self.properties["name"].setText("%s"%job_name),
-        self.properties["status"].setPixmap(self.icons[0])
+        self.icons.append(QtGui.QPixmap(os.path.join(self.current_path,"icons","OK.png")))
         
-        self._register_properties()
+        self._tab_id=QtGui.QLabel()
+        self._tab_id.setAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter)
         
-    def _register_properties(self):
-        for k,v in self.properties.iteritems():
-        	#register get_set for entries
-        	pass
+        self._tab_name=QtGui.QLabel()
+        self._tab_name.setAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter)
+        
+        self._tab_owner=QtGui.QLabel()
+        self._tab_owner.setAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter)
+        
+        self._tab_status=QtGui.QLabel()
+        self._tab_status.setAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter)
+
+        self._tab_procs=QtGui.QLabel()
+        self._tab_procs.setAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter)
+        
+        self._tab_priority=QtGui.QLabel()
+        self._tab_priority.setAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter)
+        
+        
+        
+        self._tab_id.setText("%s"%drq_job_object.id)        
+        self._tab_name.setText("%s"%drq_job_object.name)
+        self._tab_owner.setText("%s"%drq_job_object.owner)
+        self._tab_status.setPixmap( self.icons[drq_job_object.status])
+        self._tab_procs.setText("%d"%drq_job_object.nprocs)
+        self._tab_priority.setText("%d"%drq_job_object.priority)
+        
+        
         
     def add(self,table,index):
-        for job_name , widget in self.properties.iteritems():
-            widget.setAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter)
-            job_row = table.setCellWidget(index,len(self.columns),widget)
-            self.columns.append(job_row)
-            
-    def update(self,value_dict):
-        for k,v in value_dict.iteritems():
-            self.properties[k].setText(str(v))
-            if k == "status":
-            	self.properties[k].setPixmap(self.icons[v])
-
-
-        
-        
-        
+            table.setCellWidget(index,0,self._tab_id)
+            table.setCellWidget(index,1,self._tab_name)
+            table.setCellWidget(index,2,self._tab_owner)
+            table.setCellWidget(index,3,self._tab_status) 
+            table.setCellWidget(index,4,self._tab_procs) 
+            table.setCellWidget(index,7,self._tab_priority) 
--- src/drQueueUI.py
+++ src/drQueueUI.py
@@ -6,86 +6,103 @@ import PyQt4.QtCore as QtCore
 import PyQt4.uic as uic
 
 import drQueueCore as core
-
-properties={
-    "id": 0,
-    "name":"test",
-    "owner":"user",
-    "status":1,
-    "process":294232,
-    "left":"10%",
-    "done":0,
-    "pri":0,
-    "pool":0
-    }
+import drqueue.base.libdrqueue as drqueue
 
 ui_path=os.path.join(os.path.dirname(__file__),"ui","drQueueUi.ui")
 widget_class, base_class = uic.loadUiType(ui_path)
 
+
+
 class drQ(widget_class, base_class):
-	def __init__(self,*args,**kwargs):
-		super(drQ,self).__init__(*args,**kwargs)
-		self.jobs=[]
-		self.setup_main()
+    def __init__(self,*args,**kwargs):
+        super(drQ,self).__init__(*args,**kwargs)
+        self.setupUi(self)
+        self._timer_=core.Timer()
+        self.timer_interrupt=0
+        self.tab_list=[]
+        
+        self.setup_main()
+        self.PB_refresh.clicked.connect(self.refresh)
+        self.CB_auto_refresh.stateChanged.connect(self.autorefresh)
+        
+        self.connect(self._timer_,QtCore.SIGNAL("done"),self.refresh)
+        
+    def setup_main(self):
+        self.setWindowTitle("DrQueue Manager")
+        self.set_main_icons()
+        self.setup_about()
+        self.setup_jobs()
+        self.init_jobs_tabs()
+        
+    def setup_jobs(self):
+        #add a couple of jobs
+        self.TW_job.clear()
+        job_properties=["id","name","owner","status","process","left","done","priority","pool"]
 
-	@QtCore.pyqtSignature("on_exitButton_clicked()")
-	def on_exitButton_clicked(self):
-		self.close()
-		
-	@QtCore.pyqtSignature("on_Refresh_clicked()")
-	def on_Refresh_clicked(self):
-		pass
+        self.TW_job.setColumnCount(len(job_properties))
+        self.TW_job.setHorizontalHeaderLabels(job_properties) 
 
-	def setup_main(self):
-		self.setupUi(self)
-	
-		self.setWindowTitle("DrQueue Manager")
-		self.set_main_icons()
-		self.setup_about()
-		self.setup_jobs()
-		
-	def setup_jobs(self):
-		#add a couple of jobs
-		self.TW_job.clear()
+        #    set the dialog as a standard window
+        self.setWindowFlags(QtCore.Qt.Window |
+                            QtCore.Qt.WindowMinimizeButtonHint | 
+                            QtCore.Qt.WindowCloseButtonHint | 
+                            QtCore.Qt.WindowMaximizeButtonHint)
+        
+        self.TW_job.verticalHeader().hide()
+        self.TW_job.setAlternatingRowColors(True)
+        self.TW_job.setSelectionBehavior(QtGui.QTableView.SelectRows)
+        self.TW_job.setSelectionMode(QtGui.QTableView.SingleSelection)
+        
+    def refresh(self):
+        self.init_jobs_tabs()
+    
+    def autorefresh(self,status):
+        if status:
+            print "autorefresh:ON"
+            refresh_time=self.SB_refresh_time.value()
+            self._timer_.set_run_time(refresh_time)
+            self._timer_.start()
+        else:
+            print "autorefresh:OFF"
+            self._timer_.terminate()
 
-		self.TW_job.setColumnCount(len(properties.keys()))
-		self.TW_job.setHorizontalHeaderLabels(properties.keys()) 
+    def init_jobs_tabs(self):
+        print "building tabs..."
+        self.TW_job.clearContents()
+        jobs=self._get_all_jobs()
+        num_jobs = len(jobs)
+        self.TW_job.setRowCount(num_jobs)        
 
-		#	set the dialog as a standard window
-		self.setWindowFlags(QtCore.Qt.Window |
-						    QtCore.Qt.WindowMinimizeButtonHint | 
-						    QtCore.Qt.WindowCloseButtonHint | 
-						    QtCore.Qt.WindowMaximizeButtonHint)
-				
-		for i in range(100):
-			job = self.add_job()
-			job.update(properties)
-					
-	def add_job(self):
-		num_jobs = len(self.jobs)
-		self.TW_job.setRowCount(num_jobs+1)
-		job_tab = core.JobDataTab("test_%s"%num_jobs)
-		job_tab.add(self.TW_job, num_jobs)
-		self.jobs.append(job_tab)
-		return job_tab
-		
-	def setup_about(self):
-		url=QtCore.QUrl("about.html")
-		self.WV_about.load(url)
-		
-	def set_main_icons(self):
-		self.setWindowIcon(QtGui.QIcon("icons/main.svg"))
-		self.TW_main.setTabIcon(0,QtGui.QIcon("icons/job.svg"))
-		self.TW_main.setTabIcon(1,QtGui.QIcon("icons/nodes.svg"))		
-		self.TW_main.setTabIcon(2,QtGui.QIcon("icons/about.svg"))		
-		
+        for i in range(num_jobs):
+            job_tab = core.JobDataTab(jobs[i])
+            job_tab.add(self.TW_job, i)
+            self.tab_list.append(job_tab)
+        
+    def setup_about(self):
+        url=QtCore.QUrl("about.html")
+        self.WV_about.load(url)
+        
+    def set_main_icons(self):
+        self.setWindowIcon(QtGui.QIcon("icons/main.svg"))
+        self.TW_main.setTabIcon(0,QtGui.QIcon("icons/job.svg"))
+        self.TW_main.setTabIcon(1,QtGui.QIcon("icons/nodes.svg"))        
+        self.TW_main.setTabIcon(2,QtGui.QIcon("icons/about.svg"))        
+
+    def _get_all_jobs(self):
+        job_list = drqueue.request_job_list(drqueue.CLIENT)
+        return job_list
+    
+    def _get_all_slaves(self):
+        computer_list = drqueue.request_computer_list (drqueue.CLIENT)
+        print "Computers connected to the master:"
+        for computer in computer_list:
+            print "ID: %3i  Name: %s | Enabled: %s"%(computer.hwinfo.id,computer.hwinfo.name.ljust(20),(lambda x: x and "Yes" or "No")(computer.limits.enabled)) 
+
 def main():
-	app = QtGui.QApplication(sys.argv)
-	dialog = drQ()
-	print "dialog",dialog
-	
-	dialog.show()
-	return app.exec_()
+    app = QtGui.QApplication(sys.argv)
+    dialog = drQ()    
+    dialog.show()
+    return app.exec_()
 
 if __name__ == "__main__":
-	main()
+    main()
