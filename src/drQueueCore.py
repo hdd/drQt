ee4b6891a50aa45b4bdaf88091e5c3f051e304bd
langeli <langeli@hddlaptop.localdomain>
2010-12-24 11:11:23 +0000

    start on nodes

--- src/drQueueCore.py
+++ src/drQueueCore.py
@@ -6,12 +6,9 @@ import time
 from PyQt4 import QtCore
 from PyQt4 import QtGui
 
-#class Timer(threading.Thread):
 class Timer(QtCore.QThread):
     def __init__(self,parent=None):
         super(Timer,self).__init__(parent=parent)
-        self.mutex = QtCore.QMutex()
-        self.condition = QtCore.QWaitCondition()
         self.runTime = 5
         self.action=None
         self._running=True
@@ -27,19 +24,76 @@ class Timer(QtCore.QThread):
                 counter -= 1
             self.emit(QtCore.SIGNAL("done"))
 
-class JobDataTab(QtCore.QObject):
+
+
+class NodeDataTab(QtGui.QWidget):
+    def __init__(self,drq_node_object=None,parent=None):
+        super(NodeDataTab,self).__init__(parent=parent)
+        self.drq_node_object=drq_node_object
+                 
+        self.columns=[] 
+               
+        self.current_path = os.path.dirname(__file__)
+        self.icons=[]       
+        
+        self.oss=["windows","macOs","linux"]
+  
+        self.icons.append(QtGui.QPixmap(os.path.join(self.current_path,"icons","stop.png")))
+        self.icons.append(QtGui.QPixmap(os.path.join(self.current_path,"icons","ok.png")))       
+        #node_properties=["Id","Enabled","Running","Name","Os","CPUs","Load Avg","Pools"]
+        
+        self._tab_id=QtGui.QLabel()
+        self._tab_id.setAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter) 
+        
+        self._tab_enabled=QtGui.QLabel()
+        self._tab_enabled.setAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter)                
+
+        self._tab_running=QtGui.QLabel()
+        self._tab_running.setAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter)  
+               
+        self._tab_name=QtGui.QLabel()
+        self._tab_name.setAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter)
+        
+        self._tab_os=QtGui.QLabel()
+        self._tab_os.setAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter)
+
+        self._tab_cpus=QtGui.QLabel()
+        self._tab_cpus.setAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter)
+        
+        self._tab_loadavg=QtGui.QLabel()
+        self._tab_loadavg.setAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter)
+        
+        self._tab_pools=QtGui.QLabel()
+        self._tab_pools.setAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter)
+        
+        self._tab_id.setText("%d"%drq_node_object.hwinfo.id)
+        self._tab_name.setText("%s"%drq_node_object.hwinfo.name)
+        self._tab_enabled.setPixmap( self.icons[drq_node_object.limits.enabled].scaled(25,25))
+        
+        self._tab_os.setText("%s"%self.oss[drq_node_object.hwinfo.os])
+        self._tab_cpus.setText("%d"%drq_node_object.hwinfo.ncpus)
+        #self._tab_pools.setText("%s"%drq_node_object.limits.pool)
+        
+    def add(self,table,index):
+            table.setCellWidget(index,0,self._tab_id)
+            table.setCellWidget(index,1,self._tab_enabled)
+            table.setCellWidget(index,2,self._tab_running)
+            table.setCellWidget(index,3,self._tab_name) 
+            table.setCellWidget(index,4,self._tab_os) 
+            table.setCellWidget(index,5,self._tab_cpus) 
+            table.setCellWidget(index,6,self._tab_loadavg) 
+            table.setCellWidget(index,7,self._tab_pools)         
+                        
+class JobDataTab(QtGui.QWidget):
     
-    def __init__(self,drq_job_object=None):
-        super(JobDataTab,self).__init__()
+    def __init__(self,drq_job_object=None,parent=None):
+        super(JobDataTab,self).__init__(parent=parent)
         self._drq_job_object=drq_job_object
                 
-        self.icons=[]
         self.columns=[]        
         
         self.current_path = os.path.dirname(__file__)
-        
-        ##job_properties=["id","name","owner","status","process","left","done","pri","pool"]
-                
+        self.icons=[]       
         self.icons.append(QtGui.QPixmap(os.path.join(self.current_path,"icons","running.png")))  
         self.icons.append(QtGui.QPixmap(os.path.join(self.current_path,"icons","running.png")))  
         self.icons.append(QtGui.QPixmap(os.path.join(self.current_path,"icons","stop.png")))
@@ -66,15 +120,25 @@ class JobDataTab(QtCore.QObject):
         self._tab_pool=QtGui.QLabel()
         self._tab_pool.setAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter)
         
+        self._tab_est_time=QtGui.QLabel()
+        self._tab_est_time.setAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter)
+        
+        """
+            set values from drQ job instance
+        """
         
         self._tab_id.setText("%s"%drq_job_object.id)        
         self._tab_name.setText("%s"%drq_job_object.name)
         self._tab_owner.setText("%s"%drq_job_object.owner)
         self._tab_status.setPixmap( self.icons[drq_job_object.status].scaled(25,25))
         self._tab_procs.setText("%d"%drq_job_object.nprocs)
+        
+        self._tab_est_time.setText("%d"%drq_job_object.fdone)
+        
         self._tab_priority.setText("%d"%drq_job_object.priority)
         self._tab_pool.setText("%s"%drq_job_object.limits.pool)
         
+        self._tab_id.setToolTip("hey")
         
     def add(self,table,index):
             table.setCellWidget(index,0,self._tab_id)
@@ -82,5 +146,7 @@ class JobDataTab(QtCore.QObject):
             table.setCellWidget(index,2,self._tab_owner)
             table.setCellWidget(index,3,self._tab_status) 
             table.setCellWidget(index,4,self._tab_procs) 
+            table.setCellWidget(index,5,self._tab_est_time) 
+            
             table.setCellWidget(index,7,self._tab_priority) 
             table.setCellWidget(index,8,self._tab_pool) 
--- src/drQueueUI.py
+++ src/drQueueUI.py
@@ -12,21 +12,34 @@ ui_path=os.path.join(os.path.dirname(__file__),"ui","drQueueUi.ui")
 widget_class, base_class = uic.loadUiType(ui_path)
 
 
-
 class drQ(widget_class, base_class):
     def __init__(self,*args,**kwargs):
         super(drQ,self).__init__(*args,**kwargs)
-        self.setupUi(self)
-        self._timer_=core.Timer()
-        self.timer_interrupt=0
-        self.tab_list=[]
         
+        try:
+            drqueue.request_job_list(drqueue.CLIENT)
+        except:
+            raise "NO MASTER FOUND"
+        
+        
+        self.setupUi(self)
+        self._timer_=core.Timer(parent=self)
+        self.timer_interrupt=0
+        self.jobs_tab_list=[]
+        self.nodes_tab_list=[]
         self.setup_main()
         self.PB_refresh.clicked.connect(self.refresh)
         self.CB_auto_refresh.stateChanged.connect(self.autorefresh)
         
+        #    catch the "done" signal from the thread and update the table
         self.connect(self._timer_,QtCore.SIGNAL("done"),self.refresh)
         self.SB_refresh_time.setMinimum(1)
+        
+        #    set the dialog as a standard window
+        self.setWindowFlags(QtCore.Qt.Window |
+                            QtCore.Qt.WindowMinimizeButtonHint | 
+                            QtCore.Qt.WindowCloseButtonHint | 
+                            QtCore.Qt.WindowMaximizeButtonHint)        
         
     def setup_main(self):
         self.setWindowTitle("DrQueue Manager")
@@ -35,20 +48,28 @@ class drQ(widget_class, base_class):
         self.setup_jobs()
         self.init_jobs_tabs()
         
+        self.setup_nodes()
+        self.init_nodes_tabs()
+        
+    def setup_nodes(self):
+        self.TW_node.clear()
+        node_properties=["Id","Enabled","Running","Name","Os","CPUs","Load Avg","Pools"]
+        self.TW_node.setColumnCount(len(node_properties))
+        self.TW_node.setHorizontalHeaderLabels(node_properties) 
+        
+        self.TW_node.verticalHeader().hide()
+        self.TW_node.setAlternatingRowColors(True)
+        self.TW_node.setSelectionBehavior(QtGui.QTableView.SelectRows)
+        self.TW_node.setSelectionMode(QtGui.QTableView.SingleSelection) 
+                       
     def setup_jobs(self):
         #add a couple of jobs
         self.TW_job.clear()
-        job_properties=["id","name","owner","status","process","left","done","priority","pool"]
+        job_properties=["Id","Name","Owner","Status","Process","Left","Done","Priority","Pool"]
 
         self.TW_job.setColumnCount(len(job_properties))
         self.TW_job.setHorizontalHeaderLabels(job_properties) 
 
-        #    set the dialog as a standard window
-        self.setWindowFlags(QtCore.Qt.Window |
-                            QtCore.Qt.WindowMinimizeButtonHint | 
-                            QtCore.Qt.WindowCloseButtonHint | 
-                            QtCore.Qt.WindowMaximizeButtonHint)
-        
         self.TW_job.verticalHeader().hide()
         self.TW_job.setAlternatingRowColors(True)
         self.TW_job.setSelectionBehavior(QtGui.QTableView.SelectRows)
@@ -57,6 +78,8 @@ class drQ(widget_class, base_class):
     def refresh(self):
         self.setCursor(QtCore.Qt.WaitCursor);
         self.init_jobs_tabs()
+        self.init_nodes_tabs()
+        self.TW_job.repaint()
         self.setCursor(QtCore.Qt.ArrowCursor);
         
     def autorefresh(self,status):
@@ -70,17 +93,29 @@ class drQ(widget_class, base_class):
             self._timer_.terminate()
 
     def init_jobs_tabs(self):
-        self.tab_list=[]
-        print "building tabs..."
+        self.jobs_tab_list=[]
+        print "building job tabs..."
         self.TW_job.clearContents()
         jobs=self._get_all_jobs()
         num_jobs = len(jobs)
         self.TW_job.setRowCount(num_jobs)        
 
         for i in range(num_jobs):
-            job_tab = core.JobDataTab(jobs[i])
+            job_tab = core.JobDataTab(jobs[i],parent=self.TW_job)
             job_tab.add(self.TW_job, i)
-            self.tab_list.append(job_tab)
+            self.jobs_tab_list.append(job_tab)
+        
+    def init_nodes_tabs(self):
+        self.nodes_tab_list=[]
+        print "building nodes tabs..."
+        nodes=self._get_all_nodes()
+        num_nodes = len(nodes)
+        self.TW_node.setRowCount(num_nodes)
+        for i in range(num_nodes):
+            node_tab = core.NodeDataTab(nodes[i],parent=self.TW_node)
+            node_tab.add(self.TW_node, i)
+            self.nodes_tab_list.append(node_tab)
+                      
         
     def setup_about(self):
         url=QtCore.QUrl("about.html")
@@ -96,12 +131,10 @@ class drQ(widget_class, base_class):
         job_list = drqueue.request_job_list(drqueue.CLIENT)
         return job_list
     
-    def _get_all_slaves(self):
+    def _get_all_nodes(self):
         computer_list = drqueue.request_computer_list (drqueue.CLIENT)
-        print "Computers connected to the master:"
-        for computer in computer_list:
-            print "ID: %3i  Name: %s | Enabled: %s"%(computer.hwinfo.id,computer.hwinfo.name.ljust(20),(lambda x: x and "Yes" or "No")(computer.limits.enabled)) 
-
+        return computer_list
+        
 def main():
     app = QtGui.QApplication(sys.argv)
     dialog = drQ()    
